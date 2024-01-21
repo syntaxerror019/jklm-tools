c4bc8f34e3cde07e48b3b4aec2139a8ee5c0e93d
syntaxerror <142847239+syntaxerror019@users.noreply.github.com>
2024-01-21 10:05:24 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,71 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'OK'
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route('/join', methods=['POST'])
+def join():
+      json_data = request.json
+      roomCode  = json_data.get('roomCode')
+
+      if not roomCode:
+         return jsonify({"done": False, "error": "please specify all valeus"})
+          
+      try:
+            endpoint = "https://jklm.fun/api/joinRoom"
+
+            # Construct json data
+            data = {'roomCode': roomCode}
+          
+            r = requests.post(url=endpoint, json=data)
+            joinURL = r.json()['url']
+          
+            return jsonify({"done": True, "url": joinURL})
+      except Exception as e:
+             return jsonify({"done": False, "error": e})
+
+
+@app.route('/test') # Test and see if a room code is valid
+def test_room():
+    try:
+         endpoint = "https://jklm.fun/api/joinRoom"
+         data = {'roomCode': the_code}
+         r = requests.post(url=endpoint, json=data)
+         return jsonify({"done": True, "url": r.json()['url']})
+    except:
+         return jsonify({"done": True, "url": ""})
+
+
+@app.route('/create', methods=['POST']) # Create a room using requests
+def create_room():
+      json_data = request.json
+    
+      token  = json_data.get('creatorUserToken')
+      public = json_data.get('isPublic')
+      name   = json_data.get('name')
+      gameId = json_data.get('gameId')
+
+      if token and public and name and gameId:
+    
+          endpoint_create = "https://jklm.fun/api/startRoom"
+          
+          # Construct json data
+          data = {'creatorUserToken' : token,
+                            'isPublic' : public,
+                            'name' : name,
+                            'gameId' : gameId}
+          
+          res = requests.post(url = endpoint_create, json = create_data)
+          
+          if res.status_code != 200: 
+              return jsonify({"done": False, "error": res.status_code})
+              
+          code = res.json()['roomCode']
+          return jsonify({"done": True, "code": code})
+          
+       else:
+          return jsonify({"done": False, "error": "please specify all valeus"})
